976ac561b1cb7f2ffdb3fd6febdc2ff0943ef80b
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-08 20:39:05 -0300

    Revert "Configurando Mvc e conexao com banco de dados SQL Server"
    This reverts commit 507e4c543eb77b7e719db6444d5a6d8c47eed6e9.

--- Inventory/Startup.cs
+++ Inventory/Startup.cs
@@ -1,31 +1,21 @@
-using Inventory.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Inventory
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
-            services.AddDbContext<InventoryContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("Inventory"), builder =>
-                    builder.MigrationsAssembly("Inventory")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,14 +27,13 @@ namespace Inventory
             }
 
             app.UseRouting();
-            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                        name: "Default",
-                        pattern: "{Controller=Home}/{Action=Index}/{Id?}"
-                    );
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
