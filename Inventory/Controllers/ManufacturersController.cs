dbf5286cba1a33d9284b3dc8381eaaf8760e1448
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-09 11:55:46 -0300

    Completando todo o CRUD da tabela Manufacturers

--- Inventory/Controllers/ManufacturersController.cs
+++ Inventory/Controllers/ManufacturersController.cs
@@ -1,5 +1,7 @@
 ﻿using Inventory.Data;
+using Inventory.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Inventory.Controllers
 {
@@ -7,9 +9,73 @@ namespace Inventory.Controllers
     {
         private readonly InventoryContext _context;
 
+        public ManufacturersController(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View(_context.Manufacturers.OrderBy(
+                m => m.Name));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(long id)
+        {
+            return View(_context.Manufacturers.Where(
+                m => m.ManufacturerId == id).First());
+        }
+
+        [HttpGet]
+        public IActionResult Details(long id)
+        {
+            return View(_context.Manufacturers.Where(
+                m => m.ManufacturerId == id).First());
+        }
+
+        [HttpGet]
+        public IActionResult Delete(long id)
+        {
+            return View(_context.Manufacturers.Where(
+                m => m.ManufacturerId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Manufacturer manufacturer)
+        {
+            _context.Manufacturers.Add(manufacturer);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Manufacturer manufacturer)
+        {
+            _context.Manufacturers.Remove(_context.Manufacturers.Where(
+               c => c.ManufacturerId == manufacturer.ManufacturerId).First());
+            _context.Manufacturers.Add(manufacturer);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Manufacturer manufacturer)
+        {
+            _context.Manufacturers.Remove(_context.Manufacturers.Where(
+                c => c.ManufacturerId == manufacturer.ManufacturerId).First());
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
