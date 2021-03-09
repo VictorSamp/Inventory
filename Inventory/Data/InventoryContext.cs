dd3e2ec2c3238ada682865cbf8e76456ee2fb2ef
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-08 21:03:46 -0300

    Iniciando as view Iniciais do modelo Category

---
+++ Inventory/Data/InventoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
